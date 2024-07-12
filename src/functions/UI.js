84ebb515f9135622ac7558f045d2c6d33c9064f7
SwegCet <ethantruong0804@gmail.com>
2024-07-12 03:46:14 -0700

    Initial Commit

--- src/functions/UI.js
+++ src/functions/UI.js
@@ -1,5 +1,18 @@
-
+import toDoTask from './task';
 import './style.css';
+
+//class for listing tasks
+export default class listTask {
+    constructor(name) {
+        this.name = name;
+        this.projects = [];
+    }
+    //add project
+    addProject(project) {
+        this.projects.push(project);
+    }
+
+}
 
 //we want to clear the content
 function removeContent() {
---
+++ src/functions/task.js
@@ -0,0 +1,17 @@
+//here we define task class
+
+/*export default class toDoTask {
+    constructor(title, detail, date, priority, completed = false) {
+        this.title = title;
+        this.detail = detail;
+        this.date = date;
+        this.priority = priority;
+        this.completed = completed;
+    }
+
+    completedStatus() {
+        return this.completed ? 'True' : 'False';
+    }
+}
+
+*/
