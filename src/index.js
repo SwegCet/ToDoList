cae21d358d2fc27008c703c72b3b81ad5597fe80
SwegCet <ethantruong0804@gmail.com>
2024-07-12 03:45:55 -0700

    Testing if Form was properly created/styled

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import { addPopup } from './functions/add';
+import './style.css'
 //this file should contain importing from the function.js to just load up the html
+
+const addButton = document.querySelector('.add');
+
+
+addButton.addEventListener('click', () => {
+    addPopup();
+})
