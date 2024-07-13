8e4f74bd87b0c9887a9d10c35a5ec30c193dee31
SwegCet <ethantruong0804@gmail.com>
2024-07-13 02:43:56 -0700

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import { addPopup } from './functions/add';
+import { createFormTasks } from './functions/add';
 import './style.css'
 //this file should contain importing from the function.js to just load up the html
 
 const addButton = document.querySelector('.add');
 
+const content = document.querySelector('#content');
+
+//clear content function
+export function clearContent() {
+    content.textContent = '';
+}
 
 addButton.addEventListener('click', () => {
-    addPopup();
-})
+    createFormTasks();
+});
+
+
