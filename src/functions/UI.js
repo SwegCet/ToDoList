37d046470c06d71c28374255a01583c3a9676880
SwegCet <ethantruong0804@gmail.com>
2024-07-13 02:44:24 -0700

    WIP

--- src/functions/UI.js
+++ src/functions/UI.js
@@ -1,21 +1,45 @@
-import toDoTask from './task';
+import ToDoTask from './task';
+import { createFormTasks } from './add';
+import { addProject } from './add';
+import { clearContent } from '..';
 import './style.css';
 
-//class for listing tasks
-export default class listTask {
-    constructor(name) {
-        this.name = name;
-        this.projects = [];
-    }
-    //add project
-    addProject(project) {
-        this.projects.push(project);
-    }
+createFormTasks();
 
+submitButton.addEventListener('click', () => {
+    handleSubmit();
+    clearContent();
+    console.log('this worked');
+})
+
+// Function to get form data
+function getFormData() {
+    const title = document.getElementById('new-todo-title').value;
+    const details = document.getElementById('new-todo-titles').value;
+    const date = document.getElementById('new-todo-date').value;
+    const priority = document.getElementById('input[name="option"]:checked').value;
+
+    return { title, details, date, priority };
+}
+function handleSubmit() {
+    const taskForm = document.querySelector('.create-new');
+
+    taskForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        getFormData();
+
+        const newTask = new ToDoTask(getFormData());
+
+        addTaskList(newTask);
+    });
 }
 
-//we want to clear the content
-function removeContent() {
-    const content = document.querySelector('#content');
-    content.textContent = '';
+
+function addTaskList(task) {
+    console.log(`Title : ${task.title}`);
+    console.log(`Detail : ${task.detail}`);
+    console.log(`Date : ${task.date}`);
+    console.log(`Priority : ${task.priority}`);
+
 }
