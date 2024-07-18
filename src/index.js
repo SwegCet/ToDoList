3a5510977058feb2f302d9a7d5b5ca5938bc9948
SwegCet <ethantruong0804@gmail.com>
2024-07-18 03:50:22 -0700

    Loaded Task and project off loadup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
-import { createFormTasks } from './functions/add';
+import { createFormTasks, renderProject, renderTask } from './functions/add';
+import { displayProject, displayTask, filterTasks } from './functions/UI';
 import './style.css'
 //this file should contain importing from the function.js to just load up the html
 
 const addButton = document.querySelector('.add');
 
-const content = document.querySelector('#content');
+const taskContainer = document.querySelector('.task-container');
 
-//clear content function
-export function clearContent() {
-    content.textContent = '';
+const navHome = document.querySelector('#nav-home');
+
+//Initalize List of Projects and Tasks as page is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    //Call Functions
+    displayTask();
+    displayProject();
+});
+
+
+//clear taskContainer function
+export function cleartaskContainer() {
+    taskContainer.textContent = '';
 }
 
+//Pop up the form
 addButton.addEventListener('click', () => {
     createFormTasks();
 });
 
+navHome.addEventListener('click', () => {
+    displayTask();
+})
+
+//add event listener click
+document.addEventListener('DOMContentLoaded', () => {
+    let projectItems = document.querySelectorAll('.project-title');
+    projectItems.forEach(item => {
+        item.addEventListener('click', (event) => {
+            let selectedProject = event.currentTarget.getAttribute('value');
+            filterTasks(selectedProject);
+            console.log(`Clicked Value = ${selectedProject}`);
+        })
+    })
+})
 
