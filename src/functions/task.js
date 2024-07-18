367c947f2aa266c8db729837a15f826f6311e47e
SwegCet <ethantruong0804@gmail.com>
2024-07-18 03:48:19 -0700

    Added projectId property for filtering tasks

--- src/functions/task.js
+++ src/functions/task.js
@@ -1,11 +1,12 @@
 //here we define task class
 export default class ToDoTask {
-    constructor(title, detail, date, priority) {
+    constructor(title, detail, date, priority, projectId) {
         this.id = Date.now();
         this.title = title;
         this.detail = detail;
         this.date = date;
         this.priority = priority;
+        this.projectId = projectId;
     }
 }
 
