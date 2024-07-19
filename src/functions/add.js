bb907aa8403a3e9838f1bb8d8cf0b7bc2d78d773
SwegCet <ethantruong0804@gmail.com>
2024-07-19 04:58:50 -0700

    Added edit form

--- src/functions/add.js
+++ src/functions/add.js
@@ -1,7 +1,8 @@
-import { filterTasks, handleSubmit, updateProjectDropdown } from "./UI";
+import { handleSubmit, updateProjectDropdown, editTask } from "./UI";
 import { handleProjectSubmit } from "./UI";
 import { removeTask } from "./UI";
 import trashIcon from '../svgs/trash-can-outline.svg';
+import editIcon from '../svgs/file-edit-outline.svg';
 import closeIcon from '../svgs/close.svg';
 import '../style.css';
 
@@ -414,6 +415,11 @@ export function renderTask(task) {
     taskPriority.className = 'task-priority';
     taskPriority.textContent = task.priority;
 
+    const editButton = document.createElement('img');
+    editButton.setAttribute('id', 'edit');
+    editButton.src = editIcon;
+    editButton.onclick = () => editTask(task.id);
+
     const deleteButton = document.createElement('img');
     deleteButton.setAttribute('id', 'delete');
     deleteButton.src = trashIcon;
@@ -424,7 +430,235 @@ export function renderTask(task) {
     taskCard.appendChild(taskDetail);
     taskCard.appendChild(taskDate);
     taskCard.appendChild(taskPriority);
+    taskCard.appendChild(editButton);
     taskCard.appendChild(deleteButton);
 
     taskContainer.appendChild(taskCard);
 }
+
+export function openEditForm(task) {
+    const content = document.querySelector('#content');
+
+    //create the DOM for the form.
+    const form = document.createElement('form');
+    form.className = 'create-new';
+
+    //the header for the form
+    const createNewHeader = document.createElement('div');
+    createNewHeader.className = 'create-new-header';
+
+    const createTitle = document.createElement('h2');
+    createTitle.className = 'create-new-title';
+    createTitle.textContent = 'Create a New...'
+
+    const createClose = document.createElement('div');
+    createClose.className = 'create-new-close';
+
+    const xIcon = document.createElement('img');
+    xIcon.className = 'closeIconButton';
+    xIcon.src = closeIcon;
+
+    //the sidebar for the form
+    const createNewContent = document.createElement('div');
+    createNewContent.className = 'create-new-content';
+
+    const sidebar = document.createElement('div');
+    sidebar.className = 'create-new-sidebar';
+
+    const sidebarOptions = document.createElement('div');
+    sidebarOptions.className = 'create-new-options';
+
+    const toDoList = document.createElement('li');
+    toDoList.setAttribute('id', 'new-todo-link');
+    toDoList.textContent = 'To Do';
+
+    //the input section for the form
+    const newEntry = document.createElement('div'); // this is the parent of everything else below it
+    newEntry.className = 'create-new-entry';
+    newEntry.setAttribute('id', 'new-todo-menu');
+
+    //text input section
+    const titleInput = document.createElement('textarea');
+    titleInput.className = 'create-new-input';
+    titleInput.setAttribute('id', 'new-todo-title');
+    titleInput.placeholder = 'Title: '
+    titleInput.setAttribute('required', 'required');
+    titleInput.textContent = task.title;
+
+    const detailInput = document.createElement('textarea');
+    detailInput.className = 'create-new-input-big';
+    detailInput.setAttribute('id', 'new-todo-titles');
+    detailInput.placeholder = 'Details: '
+    detailInput.setAttribute('required', 'required');
+    detailInput.textContent = task.detail;
+
+    //date section
+    const dateWrapper = document.createElement('div');
+    dateWrapper.className = 'create-new-date-wrapper';
+
+    const dateContainer = document.createElement('div');
+    dateContainer.className = 'create-new-date';
+
+    const dateTitle = document.createElement('div');
+    dateTitle.className = 'create-new-date-title';
+    dateTitle.textContent = 'Due Date: ';
+
+    const dateInput = document.createElement('input');
+    dateInput.setAttribute('type', 'date');
+    dateInput.className = 'create-new-input';
+    dateInput.setAttribute('id', 'new-todo-date');
+    dateInput.setAttribute('required', 'required');
+    dateInput.value = task.date;
+
+    //add a dropdown menu for the projects folder
+    const projectFolderContainer = document.createElement('div');
+    projectFolderContainer.className = 'add-new-folder';
+
+    const projectLabel = document.createElement('label')
+    projectLabel.className = 'project-label';
+    projectLabel.textContent = 'Project Label: ';
+
+    const projectDropdown = document.createElement('select');
+    projectDropdown.className = 'drop-menu-option';
+
+    const projectOption = document.createElement('option');
+    projectOption.className = 'default-option';
+    projectOption.setAttribute('selected', 'selected')
+    projectOption.setAttribute('value', 'Home');
+    projectOption.textContent = 'Home';
+
+    //Priority/ Submit Button section
+    const prioritySubmitContainer = document.createElement('div');
+    prioritySubmitContainer.className = 'create-new-priority-wrapper-submit';
+
+    const priorityContainer = document.createElement('div');
+    priorityContainer.className = 'create-new-priority';
+
+    const priorityContainerTitle = document.createElement('div');
+    priorityContainerTitle.className = "create-new-priority-title";
+    priorityContainerTitle.textContent = 'Priority: ';
+
+    const priorityLow = document.createElement('label');
+    priorityLow.className = 'create-new-low';
+    priorityLow.textContent = 'Low';
+    const priorityLowInput = document.createElement('input');
+    priorityLowInput.setAttribute('type', 'radio');
+    priorityLowInput.setAttribute('id', 'create-new-low');
+    priorityLowInput.setAttribute('name', 'option');
+    priorityLowInput.setAttribute('value', 'Low');
+
+    //checks if the buttons work
+    priorityLow.addEventListener('click', () => {
+        console.log('low was clicked');
+    })
+
+    const priorityMed = document.createElement('label');
+    priorityMed.className = 'create-new-medium';
+    priorityMed.textContent = 'Medium';
+    const priorityMedInput = document.createElement('input');
+    priorityMedInput.setAttribute('type', 'radio');
+    priorityMedInput.setAttribute('id', 'create-new-medium');
+    priorityMedInput.setAttribute('name', 'option');
+    priorityMedInput.setAttribute('value', 'Medium');
+
+    //checks if the buttons work
+    priorityMed.addEventListener('click', () => {
+        console.log('medium was clicked');
+    })
+
+    const priorityHigh = document.createElement('label');
+    priorityHigh.className = 'create-new-high';
+    priorityHigh.textContent = 'High';
+    const priorityHighInput = document.createElement('input');
+    priorityHighInput.setAttribute('type', 'radio');
+    priorityHighInput.setAttribute('id', 'create-new-high');
+    priorityHighInput.setAttribute('name', 'option');
+    priorityHighInput.setAttribute('value', 'High');
+
+    //checks if the buttons work
+    priorityHigh.addEventListener('click', () => {
+        console.log('high was clicked');
+    })
+
+    //Iterate through the radio inputs
+    const radio = document.querySelectorAll('input[name="option"]');
+
+    radio.forEach(radio => {
+        if (radio.value === task.priority) {
+            radio.checked = true;
+        }
+    })
+
+    const submitButton = document.createElement('input');
+    submitButton.setAttribute('type', 'submit');
+    submitButton.setAttribute('id', 'todo-submit');
+    submitButton.setAttribute('value', 'Add To Do');
+    submitButton.className = 'create-new-todo-submit';
+
+    //append everything
+    //Input append
+    priorityLow.appendChild(priorityLowInput);
+    priorityMed.appendChild(priorityMedInput);
+    priorityHigh.appendChild(priorityHighInput);
+
+    priorityContainer.appendChild(priorityContainerTitle);
+
+    priorityContainer.appendChild(priorityLow);
+    priorityContainer.appendChild(priorityMed);
+    priorityContainer.appendChild(priorityHigh);
+
+    prioritySubmitContainer.appendChild(priorityContainer);
+    prioritySubmitContainer.appendChild(submitButton);
+    //date
+    dateContainer.appendChild(dateTitle);
+    dateContainer.appendChild(dateInput);
+
+    projectDropdown.appendChild(projectOption);
+    projectFolderContainer.appendChild(projectLabel);
+    projectFolderContainer.appendChild(projectDropdown);
+
+
+    dateWrapper.appendChild(dateContainer);
+    dateWrapper.appendChild(projectFolderContainer);
+
+    //prioritysubmitcontainer & input append into newEntry
+    newEntry.appendChild(titleInput);
+    newEntry.appendChild(detailInput);
+    newEntry.appendChild(dateWrapper);
+    newEntry.appendChild(prioritySubmitContainer);
+
+    //Sidebar append 
+    sidebarOptions.appendChild(toDoList);
+    sidebar.appendChild(sidebarOptions);
+    createNewContent.appendChild(sidebar);
+    createNewContent.appendChild(newEntry)
+    //Header append
+    createClose.appendChild(xIcon);
+    createNewHeader.appendChild(createTitle);
+    createNewHeader.appendChild(createClose);
+
+    //WHOLE FORM APPEND
+    form.appendChild(createNewHeader);
+    form.appendChild(createNewContent);
+
+    content.appendChild(form);
+
+    //if close button was clicked, close the form
+    const closeButton = document.querySelector('.closeIconButton');
+
+    closeButton.addEventListener('click', () => {
+        form.remove();
+        console.log('Close button was clicked');
+    });
+
+    //call function for data submission
+    form.addEventListener('submit', handleSubmit);
+
+    task.id = Date.now();
+    console.log(task.title);
+    console.log(task.id);
+
+    if (form.textContent != '') {
+        updateProjectDropdown();
+    }
+} 
